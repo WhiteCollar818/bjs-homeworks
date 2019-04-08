813344389e8d599478906eaab03e65608611cc52
WhiteCollar818 <49412408+WhiteCollar818@users.noreply.github.com>
2019-04-08 22:53:11 +0300

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -11,22 +11,22 @@ function calculateMortgage() {
 }
 function calculateTotalMortgage(percent, contribution, amount, date) {
  
-  let now = new Date();
-  let n = ((date.getMonth() - now.getMonth()) + (12 * (date.getFullYear() - now.getFullYear())));
-  let S = parseInt(amount) - parseInt(contribution);
-  let P = parseInt(percent)/100/12
-  let monthPay = (S * (P + P / (((1 + P) ** n) - 1))).toFixed(2) 
-  let totalAmount = (monthPay * n).toFixed(2)
+	let now = new Date();
+  	let n = ((date.getMonth() - now.getMonth()) + (12 * (date.getFullYear() - now.getFullYear())));
+  	let S = parseInt(amount) - parseInt(contribution);
+  	let P = parseInt(percent)/100/12
+  	let monthPay = (S * (P + P / (((1 + P) ** n) - 1))).toFixed(2) 
+  	let totalAmount = (monthPay * n).toFixed(2)
 checkInput ({
-    persent: percent,
-    contribution: contribution,
-    amount: amount
+	persent: percent,
+    	contribution: contribution,
+    	amount: amount
 })
 	return totalAmount
 
 function checkInput (name, value) {
 	for (let key in data) {         
-   if (isNaN(parseInt(data[key]))) {
-      console.log(`Параметр ${key} содержит неправильное значение ${data[key]} `)}
+   		if (isNaN(parseInt(data[key]))) {
+	console.log(`Параметр ${key} содержит неправильное значение ${data[key]} `)}
   }
 }
